7106b05b2bc6dd5ec0bb14ef31a2982c2d97e121
mate.intemann@holistic-net.de <hardrockmateji@gmail.com>
2017-12-14 15:06:31 +0100

    mongoose populate group to user

--- authenticate.js
+++ authenticate.js
@@ -13,45 +13,48 @@ var express = require('express'),
 
 router.post('/authenticate', function (req, res) {
 
-    User.findOne({
-        name: req.body.name
-    }, function (err, user) {
+    User
+        .findOne({
+            name: req.body.name
+        })
+        .populate('group')
+        .exec(function (err, user) {
 
-        if (err) {
-            throw err;
-        }
-        if (!user) {
-            res.json({ success: false, message: 'Authentication failed. User not found.' });
-        } else if (user) {
+            if (err) {
+                throw err;
+            }
+            if (!user) {
+                res.json({ success: false, message: 'Authentication failed. User not found.' });
+            } else if (user) {
 
-            // check if password matches
-            if (!passwordHash.verify(req.body.password, user.password)) {
-                res.json({ success: false, message: 'Authentication failed. Wrong password.' });
-            } else {
+                // check if password matches
+                if (!passwordHash.verify(req.body.password, user.password)) {
+                    res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+                } else {
 
-                // if user is found and password is right
-                // create a token with only our given payload
-                // we don't want to pass in the entire user since that has the password
-                const payload = {
-                    admin: user.admin
-                };
-                var token = jwt.sign(payload, app.get('superSecret'), {
-                    expiresIn: '24h' // expires in 24 hours
-                });
+                    // if user is found and password is right
+                    // create a token with only our given payload
+                    // we don't want to pass in the entire user since that has the password
+                    var payload = {
+                        admin: user.admin
+                    };
+                    var token = jwt.sign(payload, app.get('superSecret'), {
+                        expiresIn: '24h' // expires in 24 hours
+                    });
 
-                // return the information including token as JSON
-                res.json({
-                    success: true,
-                    message: 'Enjoy your token!',
-                    token: token,
-                    name: user.name,
-                    group: user.group.name
-                });
+                    // return the information including token as JSON
+                    res.json({
+                        success: true,
+                        message: 'Enjoy your token!',
+                        token: token,
+                        name: user.name,
+                        group: user.group.name
+                    });
+                }
+
             }
 
-        }
-
-    });
+        });
 });
 
 module.exports = router;
